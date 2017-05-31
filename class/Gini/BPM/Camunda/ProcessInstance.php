f29e9bbc301ef201e20d7982bbdf99ffa3ef92e0
wenjun zheng <wenjun.zheng@geneegroup.com>
2017-05-31 14:25:35 +0800

    去掉search get VariableInstance ，封装到 processinstance getVariables

--- class/Gini/BPM/Camunda/ProcessInstance.php
+++ class/Gini/BPM/Camunda/ProcessInstance.php
@@ -45,4 +45,77 @@ class ProcessInstance implements \Gini\BPM\Driver\ProcessInstance {
         $this->_fetchInstance();
         return $this->data;
     }
+
+    /**
+     * [getVariables Retrieves all variables or a a variable from the instance.]
+     * @return [array] [A JSON object of variables key-value pairs. ]
+     */
+    public function getVariables(array $criteria, $start=0, $perPage=25) {
+        $id = $this->id;
+        if (!$id) return ;
+
+        $query = [];
+        $query['processInstanceId'] = $id;
+        if ($criteria['processInstanceIdIn']) {
+            unset($query['processInstanceId']);
+            $query['processInstanceIdIn'] = $criteria['processInstanceIdIn'];
+        }
+        if ($criteria['variableName']) {
+            $result = $this->_makeQuery($criteria['variableName']);
+            if ($result['like']) {
+                $query['variableNameLike'] = $result['pattern'];
+            } else {
+                $query['variableName'] = $result['pattern'];
+            }
+        }
+        if ($criteria['taskIdIn']) {
+            $query['taskIdIn'] = $criteria['taskIdIn'];
+        }
+        if ($criteria['sorting']) {
+            $query['sorting'] = $criteria['sorting'];
+        }
+        
+        $result = $this->camunda->post("history/variable-instance?deserializeValues=false&firstResult=$start&maxResults=$perPage", $query);
+        return empty($result) ? false : $result;
+    }
+
+    private function _makeQuery($name)
+    {
+        $pos = strpos($name, '=');
+        if (!$pos) {
+            if ($pos === 0) {
+                $val = substr($name, $pos+1);
+                return ['pattern' => $val];
+            } else {
+                return ['pattern' => $name];
+            }
+        }
+        else {
+            $val = substr($name, $pos+1);
+            $pos--;
+            $opt = $name[$pos].'=';
+
+            switch ($opt) {
+                case '^=': {
+                    $pattern = $val.'%';
+                }
+                break;
+
+                case '$=': {
+                    $pattern = '%'.$val;
+                }
+                break;
+
+                case '*=': {
+                    $pattern = '%'.$val.'%';
+                }
+                break;
+            }
+
+            return [
+                'like' => true,
+                'pattern' => $pattern
+            ];
+        }
+    }
 }
